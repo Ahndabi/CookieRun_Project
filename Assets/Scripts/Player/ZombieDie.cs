eefa5cd701c11e56fcb4ad39644a597bb016d0ef
안다비 안다비 <dksekqkr1234@naver.com>
2023-10-12 20:48:32 +0900

    add : no takeDamage

--- Assets/Scripts/Player/ZombieDie.cs
+++ Assets/Scripts/Player/ZombieDie.cs
@@ -1,11 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class ZombieDie : PlayerDie
 {
     bool oneDie = false;
+    ZombieTakeDamage takeDamage;
+
+    private void Awake()
+    {
+        base.Awake();
+        takeDamage = GetComponent<ZombieTakeDamage>();
+    }
 
     private void OnDisable()
     {
@@ -32,6 +40,7 @@ public class ZombieDie : PlayerDie
 
     public void ZombieSpawn()      // Spawn 애니메이션이 끝날 때 붙여줄 이벤트
     {
+        StartCoroutine(NoTakeDamageRoutine());
         GameManager.Data.ZombieSpawnHp();
 
         Time.timeScale = 1.0f;
@@ -39,5 +48,12 @@ public class ZombieDie : PlayerDie
         StopCoroutine(ShowGameResultUI());
         inputSystem.enabled = true;
         anim.SetTrigger("Move");
+    }
+
+    IEnumerator NoTakeDamageRoutine()   // 데미지를 2초 동안 받지 않는 함수
+    {
+        Destroy(takeDamage);
+        yield return new WaitForSeconds(2f);
+        gameObject.AddComponent<ZombieTakeDamage>();
     }
 }
